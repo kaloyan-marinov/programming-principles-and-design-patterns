9b9b1faaf335f878709c0f29ec5a24a782a8b3e0
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2025-05-29 17:00:38 +0200

    add `2025-03-30-14-23-01-builder-pattern.ts`

---
+++ 2025-03-30-14-23-01-builder-pattern.ts
@@ -0,0 +1,27 @@
+class HTTPRequest {
+  constructor(
+    public url: string,
+    public method: string,
+    public headers: Record<string, string>,
+    public queryParams: Record<string, any>,
+    public body: any,
+    public timeout: number,
+    public retries: number,
+    public validateStatus: boolean,
+    public cache: boolean | undefined,
+    public followRedirect: boolean
+  ) {}
+}
+
+const request = new HTTPRequest(
+  "https://api.example.com",
+  "POST",
+  { authorization: " Bearer token" },
+  { userId: 123 },
+  { name: "John" },
+  30000,
+  3,
+  true,
+  undefined,
+  false
+);
