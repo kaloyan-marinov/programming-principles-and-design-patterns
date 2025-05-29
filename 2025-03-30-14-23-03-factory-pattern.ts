e0d075fce4f9c2c8de68b369d83fd07d97791f0f
Kaloyan Marinov <happy-2025-04-06-14-33@protonmail.com>
2025-05-29 17:40:32 +0200

    build out `2025-03-30-14-23-03-factory-pattern.ts` (resolving several problems)

--- 2025-03-30-14-23-03-factory-pattern.ts
+++ 2025-03-30-14-23-03-factory-pattern.ts
@@ -1,3 +1,40 @@
+export class User {
+  private id: number;
+  private name: string;
+
+  constructor(data: any) {
+    this.id = data.id;
+    this.name = data.name;
+  }
+}
+
+export class AdminUser extends User {
+  private type: string;
+
+  constructor(data: any) {
+    super(data);
+    this.type = "admin";
+  }
+}
+
+export class ModeratorUser extends User {
+  private type: string;
+
+  constructor(data: any) {
+    super(data);
+    this.type = "moderator";
+  }
+}
+
+export class RegularUser extends User {
+  private type: string;
+
+  constructor(data: any) {
+    super(data);
+    this.type = "regular";
+  }
+}
+
 export const type = "admin";
 export const data = {
   id: 1,
@@ -11,5 +48,5 @@ if (type === "admin") {
 } else if (type === "moderator") {
   user = new ModeratorUser(data);
 } else {
-  user = RegularUser(data);
+  user = new RegularUser(data);
 }
